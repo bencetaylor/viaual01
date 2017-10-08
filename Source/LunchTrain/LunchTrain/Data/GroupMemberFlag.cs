aaa79c1a58a90975bb8a13cac4c98201bd6ef561
Kárpáti Zoltán <karpati.z@upcmail.hu>
2017-10-08 15:54:23 +0200

    Classes
    GroupMemberFlag, GroupMembership

---
+++ Source/LunchTrain/LunchTrain/Data/GroupMemberFlag.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace LunchTrain.Data
+{
+    public class GroupMemberFlag
+    {
+        [Key, Column(Order = 1)]
+        public string GroupID { get; set; }
+        [Key, Column(Order = 2)]
+        public string UserID { get; set; }
+        public int Status { get; set; }
+
+        [ForeignKey("GroupID")]
+        public virtual Group Group { get; set; }
+        [ForeignKey("UserID")]
+        public virtual ApplicationUser ApplicationUser { get; set; }
+    }
+}
---
+++ Source/LunchTrain/LunchTrain/Data/GroupMembership.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace LunchTrain.Data
+{
+    public class GroupMembership
+    {
+        [Key, Column(Order = 1)]
+        public string GroupID { get; set; }
+        [Key, Column(Order = 2)]
+        public string UserID { get; set; }
+
+        [ForeignKey("GroupID")]
+        public virtual Group Group { get; set; }
+        [ForeignKey("UserID")]
+        public virtual ApplicationUser ApplicationUser { get; set; }
+    }
+}
