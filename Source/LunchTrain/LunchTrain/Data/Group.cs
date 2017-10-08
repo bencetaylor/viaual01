8d7bc22ad921eceb72b118bcf36d613e4834fdc7
Jákli Aida <aidajakli@gmail.com>
2017-10-08 16:56:49 +0200

    Group, GroupApplication kiegészítés

--- Source/LunchTrain/LunchTrain/Data/Group.cs
+++ Source/LunchTrain/LunchTrain/Data/Group.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,8 +9,13 @@ namespace LunchTrain.Data
 {
     public class Group
     {
-        public string Name;
-        public string Description;
-        public string Owner;
+        [Key]
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+        public string Owner { get; set; }
+
+        [ForeignKey("Owner")]
+        public ApplicationUser ApplicationUser { get; set; }
     }
 }
--- Source/LunchTrain/LunchTrain/Data/GroupApplication.cs
+++ Source/LunchTrain/LunchTrain/Data/GroupApplication.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,7 +9,15 @@ namespace LunchTrain.Data
 {
     public class GroupApplication
     {
-        public string GroupID;
-        public string UserID;
+        [Key, Column(Order = 1)]
+        public string GroupID { get; set; }
+
+        [Key, Column(Order = 2)]
+        public string UserID { get; set; }
+
+        [ForeignKey("GroupID")]
+        public Group Group { get; set; }
+        [ForeignKey("UserID")]
+        public ApplicationUser ApplicationUser { get; set; }
     }
 }
