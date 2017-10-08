50d75c8913142133b00a755739b8eadd475e939f
Jákli Aida <aidajakli@gmail.com>
2017-10-08 15:21:06 +0200

    Group, GroupApplication

---
+++ Source/LunchTrain/LunchTrain/Data/Group.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LunchTrain.Data
+{
+    public class Group
+    {
+        public string Name;
+        public string Description;
+        public string Owner;
+    }
+}
