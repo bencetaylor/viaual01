a087e80623910ea9fc8b1b459cff811b84a1584d
LENOVO-Y700\Bence <bencetaylor@gmail.com>
2017-10-30 21:07:34 +0100

    Group/Index feltétel hozzáadva

--- Source/LunchTrain/LunchTrain/Pages/Groups/Index.cshtml.cs
+++ Source/LunchTrain/LunchTrain/Pages/Groups/Index.cshtml.cs
@@ -6,23 +6,28 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using LunchTrain.Data;
+using Microsoft.AspNetCore.Identity;
 
 namespace LunchTrain.Pages.Groups
 {
     public class IndexModel : PageModel
     {
         private readonly ApplicationDbContext _context;
+        private readonly UserManager<ApplicationUser> _userManager;
 
-        public IndexModel(ApplicationDbContext context)
+        public IndexModel(ApplicationDbContext context, UserManager<ApplicationUser> userManager)
         {
             _context = context;
+            _userManager = userManager;
         }
 
         public IList<Group> Group { get;set; }
+        public ApplicationUser user { get; set; }
 
         public async Task OnGetAsync()
         {
             Group = await _context.Groups.Include(x => x.Owner).ToListAsync();
+            user = (await _userManager.GetUserAsync(HttpContext.User));
         }
     }
 }
