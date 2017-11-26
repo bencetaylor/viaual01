ae593232ecac11f91c5d7da63bb4b433584ac2d0
Kárpáti Zoltán <karpati.z@upcmail.hu>
2017-11-26 23:38:24 +0100

    GroupMemberComponent/Apply
    Apply gomb még nem müködik, linkek valamiért nem jók.

--- Source/LunchTrain/LunchTrain/Pages/Groups/Index.cshtml.cs
+++ Source/LunchTrain/LunchTrain/Pages/Groups/Index.cshtml.cs
@@ -23,14 +23,59 @@ namespace LunchTrain.Pages.Groups
 
         public IList<Group> Group { get;set; }
         public ApplicationUser user { get; set; }
+        public IList<Group> UserMemberGroups { get; set; }
+        public IList<Group> UserApplicationGroup { get; set; }
+
+        [BindProperty]
+        public Group Input { get; set; }
+
+        public async Task<IActionResult> OnPostAsync(string id)
+        {
+
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Input = await _context.Groups.FindAsync(id);
+            var currentuser = await _userManager.GetUserAsync(HttpContext.User);
+
+            if (Input != null)
+            {
+
+                GroupApplication groupApplication = new GroupApplication();
+                groupApplication.Group = Input;
+                groupApplication.User = currentuser;
+                groupApplication.GroupID = Input.Name;
+                groupApplication.UserID = currentuser.Id;
+
+                _context.GroupApplications.Add(groupApplication);
+
+                await _context.SaveChangesAsync();
+
+            }
+
+            return RedirectToPage("./Index");
+        }
 
         public async Task OnGetAsync()
         {
-            Group = new List<Group>();
+            Group = await _context.Groups.Include(x => x.Owner).ToListAsync();
             user = await _userManager.GetUserAsync(HttpContext.User);
+            
+            UserMemberGroups = new List<Group>();
             foreach (var member in _context.GroupMemberships) {
                 if (member.UserID == user.Id) {
-                    Group.Add(await _context.Groups.Include(x => x.Owner).SingleOrDefaultAsync(m => m.Name == member.GroupID));
+                    UserMemberGroups.Add(await _context.Groups.Include(x => x.Owner).SingleOrDefaultAsync(m => m.Name == member.GroupID));
+                }
+            }
+
+            UserApplicationGroup = new List<Group>();
+            foreach (var member in _context.GroupApplications)
+            {
+                if (member.UserID == user.Id)
+                {
+                    UserApplicationGroup.Add(await _context.Groups.Include(x => x.Owner).SingleOrDefaultAsync(m => m.Name == member.GroupID));
                 }
             }
         }
---
+++ Source/LunchTrain/LunchTrain/Views/Shared/Components/GroupMemberComponent/GroupMemberComponent.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using LunchTrain.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+
+namespace LunchTrain.Views.Shared.Components.GroupMemberComponent
+{
+
+    public class GroupMemberViewModel
+    {
+        public string UserId { get; set; }
+
+        public List<Group> UserMemberGroup { get; set; }
+    }
+
+    [ViewComponent(Name = "GroupMemberComponent")]
+    public class GroupMemberComponent : ViewComponent
+    {
+
+        private readonly ApplicationDbContext _context;
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public GroupMemberComponent(ApplicationDbContext context, UserManager<ApplicationUser> userManager)
+        {
+            _context = context;
+            _userManager = userManager;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var currentUserId = (await _userManager.GetUserAsync(HttpContext.User)).Id;
+
+            List<Group> memberGroups = new List<Group>();
+            foreach (var member in _context.GroupMemberships)
+            {
+                if (member.UserID == currentUserId)
+                {
+                    memberGroups.Add(await _context.Groups.Include(x => x.Owner).SingleOrDefaultAsync(m => m.Name == member.GroupID));
+                }
+            }
+
+            var vm = new GroupMemberViewModel {
+                UserId = currentUserId,
+                UserMemberGroup = memberGroups
+            };
+            return View(vm);
+        }
+    }
+}
