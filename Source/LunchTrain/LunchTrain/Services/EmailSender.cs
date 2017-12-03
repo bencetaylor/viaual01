121d63a52ba803bf11af0b6f6688406bf6a570f3
Tamás Deme <tomzorz@outlook.com>
2017-12-03 23:27:57 +0100

    #8 mail küldés tesztelve

--- Source/LunchTrain/LunchTrain/Services/EmailSender.cs
+++ Source/LunchTrain/LunchTrain/Services/EmailSender.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using MailKit.Net.Smtp;
+using MailKit.Security;
 using MimeKit;
 
 namespace LunchTrain.Services
@@ -12,7 +13,7 @@ namespace LunchTrain.Services
     // For more details see https://go.microsoft.com/fwlink/?LinkID=532713
     public class EmailSender : IEmailSender
     {
-        private const string MailUser = "";
+        private const string MailUser = "lunchtrain@outlook.com";
         private const string MailPass = "";
         // todo actual user pass on outlook.com
 
@@ -27,11 +28,12 @@ namespace LunchTrain.Services
                     Text = message
                 }
             };
-            msg.From.Add(new MailboxAddress(Encoding.UTF8, "LunchTrain", "noreply@lunchtrain.com"));
+            msg.From.Add(new MailboxAddress(Encoding.UTF8, "LunchTrain", "lunchtrain@outlook.com"));
             msg.To.Add(new MailboxAddress(Encoding.UTF8, "LunchTrain user", email));
             using (var client = new SmtpClient())
             {
-                client.Connect("smtp.live.com", 587);
+                client.Connect("smtp-mail.outlook.com", 587, SecureSocketOptions.StartTls);
+                client.AuthenticationMechanisms.Remove("XOAUTH2");
                 client.Authenticate(MailUser, MailPass);
                 await client.SendAsync(msg);
                 await client.DisconnectAsync(true);
